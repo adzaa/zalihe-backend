d8f115cca8b56f32e9c0de3d233601618bea49d8
adzaa <adidzankic@gmail.com>
2023-01-16 20:58:55 +0100

    db changed

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("dbzalihe", "root", "", {
+const db = new Sequelize("dbzalihe_157", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
